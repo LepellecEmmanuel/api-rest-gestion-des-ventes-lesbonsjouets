97baa5dd4b88a358363166df5b5077b0f03639b6
EmmanuelLepellec <emmanuel.lepellec@orange.fr>
2025-10-06 01:51:33 +0200

    initial commit

---
+++ src/main/java/com/asso/lesbonsjouets/gestionDesVentes/GestionDesVentesApplication.java
@@ -0,0 +1,13 @@
+package com.asso.lesbonsjouets.gestionDesVentes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDesVentesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDesVentesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asso/lesbonsjouets/gestionDesVentes/GestionDesVentesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asso.lesbonsjouets.gestionDesVentes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDesVentesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
